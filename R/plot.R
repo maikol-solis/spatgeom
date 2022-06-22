d4e0b27b5c4ae0be70c563d375ca1a4a53d9b28a
Maikol Solís <maikol.solis@ucr.ac.cr>
2022-06-22 09:02:26 -0600

    refactor: format and pacify checks
    The changes are:
    - Rename `Xdat` -> `x`
    - Pacify unbinding variables checks
    - Add missing ggplot2 namespace
    - Use `subset` instead of `%>%` + `filter`.

--- R/plot.R
+++ R/plot.R
@@ -18,54 +18,57 @@ plot_geom_curves <-
   function(x,
            type = c("curve", "deriv"),
            font_size = 12) {
+    # Number of variables
     nvar <- length(x$results)
-    df <- NULL
-    df_null <- NULL
-    df_f <- NULL
-    df_fp <- NULL
+
+    # variables bindings for R CMD check
+    df <- df_fp <- alpha <- geom_corr <- variable <- y <- NULL
+
     for (k in 1:nvar) {
-      df <-  rbind(
+      df <- rbind(
         cbind(
           x$results[[k]]$data_frame_triangles,
-          variable = colnames(x$Xdat)[k],
+          variable = colnames(x$x)[k],
           intensity = x$results[[k]]$intensity
         ),
         df
       )
 
-      df_fp <-
-        rbind(data.frame(
-          x = x$results[[k]]$data_frame_triangles$alpha[-1],
-          y = diff(x$results[[k]]$data_frame_triangles$geom_corr)/diff(x$results[[k]]$data_frame_triangles$alpha),
-          variable = colnames(x$Xdat)[k]
-        ), df_fp)
 
+      df_fp <- rbind(data.frame(
+        x = x$results[[k]]$data_frame_triangles$alpha[-1],
+        y = diff(x$results[[k]]$data_frame_triangles$geom_corr) /
+          diff(x$results[[k]]$data_frame_triangles$alpha),
+        variable = colnames(x$x)[k]
+      ), df_fp)
     }
 
     if (type == "curve") {
       plt <- ggplot2::ggplot(df) +
         ggplot2::geom_step(ggplot2::aes(x = alpha, y = geom_corr),
-                           # color = "black",
-                            size = 1)
-        # ggplot2::geom_line(data = df_f, ggplot2::aes(x, y), size = 0.5)
+          size = 1
+        )
 
       for (k in 1:nvar) {
         plt <- plt +
           ggplot2::geom_function(
-            data = subset(df, variable == colnames(Xdat)[k]),
+            data = subset(df, variable == colnames(x)[k]),
             fun = function(x, intensity) {
-              exp(-intensity * pi * x ^ 2)
+              exp(-intensity * pi * x^2)
             },
             args = list(intensity = x$results[[k]]$intensity),
             linetype = "dashed",
-            color = "red", size=1
+            color = "red", size = 1
           )
       }
 
 
-     plt <- plt +
+      plt <- plt +
         ggplot2::scale_y_continuous(name = expression(f(alpha))) +
-        ggplot2::scale_x_continuous(name = expression(alpha), labels = scales::comma) +
+        ggplot2::scale_x_continuous(
+          name = expression(alpha),
+          labels = scales::comma
+        ) +
         ggplot2::facet_wrap(. ~ variable, scales = "free_x") +
         cowplot::theme_cowplot(font_size = font_size) +
         cowplot::background_grid(minor = "y") +
@@ -74,39 +77,37 @@ plot_geom_curves <-
       plt <- ggplot2::ggplot(df) +
         ggplot2::geom_line(data = df_fp, ggplot2::aes(x, y), size = 1) +
         ggplot2::scale_y_continuous(name = expression(f * minute(alpha))) +
-        ggplot2::scale_x_continuous(name = expression(alpha), labels = scales::comma) +
+        ggplot2::scale_x_continuous(
+          name = expression(alpha),
+          labels = scales::comma
+        ) +
         ggplot2::facet_wrap(. ~ variable, scales = "free_x") +
         cowplot::theme_cowplot(font_size = font_size) +
         cowplot::background_grid(minor = "y") +
         cowplot::panel_border()
-      # ggplot2::theme(panel.grid.minor.y = ggplot2::element_line(color = "grey85", linetype = "dashed"))
-
     }
 
     return(plt)
-
   }
 
 
 #' @export
 plot_alpha_shape <- function(x, alpha, font_size = 12) {
   nvar <- length(x$results)
-  df <- NULL
-  df_whole <- NULL
+  df <- max_length <- geometry <- NULL
 
   for (k in 1:nvar) {
-    df_triangles <- x$results[[k]]$triangles %>%
-      dplyr::filter(max_length < 2 * alpha[k])
+    df_triangles <- subset(x$results[[k]]$triangles, max_length < 2 * alpha[k])
 
-    df <-  rbind(cbind(df_triangles,
-                       variable = colnames(x$Xdat)[k]), df)
-
+    df <- rbind(cbind(df_triangles,
+      variable = colnames(x$x)[k]
+    ), df)
   }
 
   ggplot2::ggplot() +
     ggplot2::geom_sf(
       data = df,
-      mapping = aes(geometry = geometry),
+      mapping = ggplot2::aes(geometry = geometry),
       fill = "skyblue1",
       color = "grey30",
       size = 0.05
@@ -131,8 +132,4 @@ plot_alpha_shape <- function(x, alpha, font_size = 12) {
   #   gganimate::transition_time(alpha_reveal)
   #
   # gganimate::animate(p)
-
 }
-
-
-
